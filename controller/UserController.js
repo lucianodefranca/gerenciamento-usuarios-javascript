0c42e467afc1ff130b649046f63f21028d9df638
luciano de franca <lucianodefranca@gmail.com>
2022-10-26 22:59:03 -0300

    Validando campos Formulario

--- controller/UserController.js
+++ controller/UserController.js
@@ -20,6 +20,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then(
                 (content) => {
 
@@ -82,8 +84,15 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
         
         [...this.formEl.elements].forEach(function(field, index) {
+
+            if (["name", "email", "password"].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
 
             if (field.name == "gender") {
 
@@ -97,6 +106,10 @@ class UserController {
                 user[field.name] = field.value;
             }
         });
+
+        if (!isValid) {
+            return false;
+        }
 
         var objectUser = new User(
             user.name, 
